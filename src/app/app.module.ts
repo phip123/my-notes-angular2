f4101ec0904a00c65ea5eb2aafef6d84f7382130
Philipp <poepsi39@gmail.com>
2016-11-20 20:32:40 +0100

    Categories and Note Component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
